d3fce2ae113b41a1d2ad2277dc853d2824516306
Brian Mungai <bryanmungai254@gmail.com>
2022-10-27 15:31:12 +0300

    imported model to view and used object method to fetch data from model

--- base/views.py
+++ base/views.py
@@ -1,24 +1,23 @@
 from django.shortcuts import render
+from .models import Room
 
 
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name': "Lets learn Python"},
-    {'id': 2, 'name': "Django is nice"},
-    {'id': 3, 'name': "I love React "},
-]
+# rooms = [
+#     {'id': 1, 'name': "Lets learn Python"},
+#     {'id': 2, 'name': "Django is nice"},
+#     {'id': 3, 'name': "I love React "},
+# ]
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-    context = {"room": room}
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
     return render(request, 'base/room.html', context)
