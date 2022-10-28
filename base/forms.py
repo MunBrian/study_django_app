50c738523c683d4f97df955690f685f3df90509a
Brian Mungai <bryanmungai254@gmail.com>
2022-10-28 13:55:19 +0300

    created room_form template. Added forms.py to handle form submit and checked for validity in view.py

---
+++ base/forms.py
@@ -0,0 +1,11 @@
+# model form
+
+from dataclasses import fields
+from django.forms import ModelForm
+from .models import Room
+
+
+class RoomForm(ModelForm):
+    class Meta:
+        model = Room
+        fields = '__all__'
--- base/models.py
+++ base/models.py
@@ -26,6 +26,10 @@ class Room(models.Model):
     # add date model once when room is initially created
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        # order in descending newest first to oldest
+        ordering = ['-updated', '-created']
+
     def __str__(self):
         return self.name
 
