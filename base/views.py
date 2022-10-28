02618bfd5ed3d190c54b1c7e954cac58c801e43d
Brian Mungai <bryanmungai254@gmail.com>
2022-10-28 22:40:53 +0300

    created a input search to search across topics, rooms.. imported Q class to help filter across multiple attributes

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -11,32 +12,36 @@ from .forms import RoomForm
 #     {'id': 3, 'name': "I love React "},
 # ]
 
-# get all data from DB
+# get data from db
 def home(request):
     # get query value from the url
     q = request.GET.get('q') if request.GET.get('q') != None else ''
 
     # filter room by topic - (__)means going up
     # icontains - ensures that whatever value in topic name atleast contains what is in the query value - case insensitive
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
 
     topics = Topic.objects.all()
 
-    context = {'rooms': rooms, 'topics': topics}
+    room_count = rooms.count()  # get number of rooms
+
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
 
     return render(request, 'base/home.html', context)
 
+
 # get requested rooms func
-
-
 def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
 
+
 # create func
-
-
 def create_room(request):
     form = RoomForm()
 
